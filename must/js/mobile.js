f4d4a3f50f4302d15250e8f2e4c7a79e8cb5dfd1
yabhinaykumar <91415853+yabhinaykumar@users.noreply.github.com>
2023-04-27 19:53:10 +0530

    Update mobile.js

--- must/js/mobile.js
+++ must/js/mobile.js
@@ -1,12 +1,12 @@
 window.onload = function(){
-	var getNavi = document.getElementById('navigation');
+	const getNavi = document.getElementById('navigation');
 
-	var mobile = document.createElement("span");
+	const mobile = document.createElement("span");
 	mobile.setAttribute("id","mobile-navigation");
 	getNavi.parentNode.insertBefore(mobile,getNavi);
 
 	document.getElementById('mobile-navigation').onclick = function(){
-		var a = getNavi.getAttribute('style');
+		const a = getNavi.getAttribute('style');
 		if(a){
 			getNavi.removeAttribute('style');
 			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile-menu.png)';
@@ -15,18 +15,18 @@ window.onload = function(){
 			document.getElementById('mobile-navigation').style.backgroundImage='url(images/mobile-close.png)';
 		}
 	};
-	var getElm = getNavi.getElementsByTagName("LI");
-	for(var i=0;i<getElm.length;i++){
+	const getElm = getNavi.getElementsByTagName("LI");
+	for(const i=0;i<getElm.length;i++){
 		if(getElm[i].children.length>1){
-			var smenu = document.createElement("span");
+			const smenu = document.createElement("span");
 			smenu.setAttribute("class","mobile-submenu");
 			smenu.setAttribute("OnClick","submenu("+i+")");
 			getElm[i].appendChild(smenu);
 		};
 	};
 	submenu = function (i){
-		var sub = getElm[i].children[1];
-		var b = sub.getAttribute('style');
+		const sub = getElm[i].children[1];
+		const b = sub.getAttribute('style');
 		if(b){
 			sub.removeAttribute('style');
 			getElm[i].lastChild.style.backgroundImage='url(images/mobile-expand.png)';
